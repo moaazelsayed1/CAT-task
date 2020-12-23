92adddd1d9e1ecd176b15031806f023d3f93be2c
moaazelsayed1 <moaazelsayed@LAPTOP-CMOT0FK3.localdomain>
2020-12-23 17:05:13 +0200

    added problem two

---
+++ reversedString.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include<string>
+
+std::string reverse(std::string p);
+
+int main()
+{
+    std::string paragraph;
+    std::getline(std::cin, paragraph);
+    paragraph = reverse(paragraph);
+
+    for (int i = 0; i < paragraph.length(); i++)
+    {
+        std::cout << paragraph[i];
+    }
+    std::cout << std::endl; 
+    
+}
+
+
+std::string reverse(std::string p)
+{
+    // we only need to iterate over thr half of the array so we can
+    // access every element only once and replace it with its index comlement 
+    for (int i = 0; i < p.length() / 2; i++)
+    {
+        char tmp = p[i];
+        p[i] = p[(p.length() - 1) - i];
+        p[(p.length() - 1) - i] = tmp;
+    }
+    return p;
+}
